ade68a359f7d1100a350d90dbaaa6740053b808a
mikekondo <foad5969@gmail.com>
2023-01-20 16:46:39 +0900

    first commit

---
+++ MiniApp-SwiftUI-MVVMTodoApp/MiniApp_SwiftUI_MVVMTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp_SwiftUI_MVVMTodoAppApp.swift
+//  MiniApp-SwiftUI-MVVMTodoApp
+//
+//  Created by 近藤米功 on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp_SwiftUI_MVVMTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
